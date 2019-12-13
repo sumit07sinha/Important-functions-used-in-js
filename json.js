ebc3fd91bc2af0de5db5b76edbb752eabe4b83f6
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2019-12-13 10:32:54 +0530

    Create json.js

---
+++ json.js
@@ -0,0 +1,8 @@
+/*JSON stands for JavaScript Object Notation.
+JSON is a lightweight data interchange format.jSON is a format for storing and transporting data.
+JSON is often used when data is sent from a server to a web page.
+When exchanging data between a browser and a server, the data can only be text.
+JSON is text, and we can convert any JavaScript object into JSON, and send JSON to the server.
+We can also convert any JSON received from the server into JavaScript objects.
+This way we can work with the data as JavaScript objects, with no complicated parsing and translations.*/
+
