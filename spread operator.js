182abfcda1cccd713d272503a1563abeccd6a414
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2019-12-17 15:21:32 +0530

    Update spread operator.js

--- spread operator.js
+++ spread operator.js
@@ -3,7 +3,8 @@ let fruits=[kiwi, oranges, apple];
 console.log(fruits);// kiwi, oranges, apple
 /*By using the spread operator we made sure that the original array is not affected whenever we alter the new array.
 The concat() method provided by javascript helps in concatenation of two or more strings(String concat() ) or is used to merge 
-two or more arrays. In case of arrays,this method does not change the existing arrays but instead returns a new array.*/
+two or more arrays. In case of arrays,this method does not change the existing arrays but instead returns a new array. 
+In case of spread you can change the existing array in to new array and put the things at a required place. */
 
 let meats=[chicken,pork];
 let fruits=[kiwi,...meats, oranges, apple];
