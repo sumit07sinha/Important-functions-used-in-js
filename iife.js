6af4daabf6ec7aa2b0d2d9a166f68628a80bf539
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2019-12-18 11:11:48 +0530

    immediately invoked function expression(IIFE)

---
+++ iife.js
@@ -0,0 +1,6 @@
+/*An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
+var name = 'sumit'; // it's not recommended to define variable in window scope*/
+/*do some stuffs*/
+(function() {
+var name="Sumit";
+})(); // it's always recommended to define a variable in the function scope not in the global scope (window scope).
