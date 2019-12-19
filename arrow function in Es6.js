d6344ae66dcefe211ea9e4af30dc1d71ed6dec26
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2019-12-19 14:40:42 +0530

    Update arrow function in Es6.js

--- arrow function in Es6.js
+++ arrow function in Es6.js
@@ -3,7 +3,14 @@ function sayHello(name){
     return 'Hello'+ name;
 }
 sayHello(Sumit)*/
-
+// Arrow function
 
 sayHello= (name) => {return 'Hello' +name};
 sayHello(Sumit);
+
+// IIFE
+
+// (function sayHello{
+//     return 'Hello'+ name;
+// }());
+
