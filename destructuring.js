f085aba96d6763ffaf38583b474efda113fe7fcb
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-01-03 19:40:21 +0530

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,8 @@
-Destructuring simply implies breaking down a complex structure into simpler parts. 
+/*Destructuring simply implies breaking down a complex structure into simpler parts. 
 In JavaScript, this complex structure is usually an object or an array.
 With the destructuring syntax, you can extract smaller fragments from arrays and objects. 
 Destructuring syntax can be used for variable declaration or variable assignment. 
-You can also handle nested structures by using nested destructuring syntax.
+You can also handle nested structures by using nested destructuring syntax.*/
 const student = {
     firstname: 'Sumit',
     lastname: 'Sinha',
