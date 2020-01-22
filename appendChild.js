9c4183afeeac91c11f477d657da8d326178d603e
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-01-22 13:45:02 +0530

    Update appendChild.js

--- appendChild.js
+++ appendChild.js
@@ -1,4 +1,4 @@
 var node = document.createElement("LI");                 // Create a <li> node
 var textnode = document.createTextNode("Water");         // Create a text node
-node.appendChild(textnode);                              // Append the text to <li>
+node.appendChild(textnode);                              // Append the text to <li> tag
 document.getElementById("myList").appendChild(node); 
