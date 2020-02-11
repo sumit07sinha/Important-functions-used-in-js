97d86a83667ce23fcff1c768ac50af18d4c758c0
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-11 12:20:14 +0530

    Update higherOrderFunction.js

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,17 +1,22 @@
-/*A higher order function is a function that takes a function as an argument, or returns a function .*/
-//following higher order functions
-/*array.map*/
-let arr=[2,4,6];
-let multiplier=(x)=>{
-return x*2;
-} // output will be [4,8,12]
-console.log(arr.map(multiplier));
+/*A higher order function is a function that takes a function as an argument, or returns a function 
+following higher order functions
+array.map*/
+    let arr=[2,4,6];
+    let multiplier=(x)=>{
+        return x*2;
+        } // output will be [4,8,12]
+     console.log(arr.map(multiplier));
 //array.fill used to fill empty array.
-let arr= new Array(100);
-let val= arr.fill(1).map(Math.random);
-console.log(val); 
+    let arr= new Array(100);
+    let val= arr.fill(1).map(Math.random);
+    console.log(val); 
 /*array. reduce 
 using  it for finding max element in an array*/\
-let vals=[2,4,8,5,7];
-let maxVal= vals.reduce((a,b)=>a>b?a:b);
-console.log(maxVal);//8
+    let vals=[2,4,8,5,7];
+    let maxVal= vals.reduce((a,b)=>a>b?a:b);
+    console.log(maxVal);//8
+//array.filter The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+    let vals=[2,4,8,5,7];
+    let maxVal= vals.filter(x=>x>2);
+    console.log(maxVal);
