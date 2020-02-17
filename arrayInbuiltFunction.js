04ef13062f42efa2f23ef0658f5571dfaab5c95f
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-17 10:28:43 +0530

    array.join() function

--- arrayInbuiltFunction.js
+++ arrayInbuiltFunction.js
@@ -2,3 +2,9 @@
 
 let fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("demo").innerHTML = fruits.toString(); //Banana,Orange,Apple,Mango
+
+/*The join() method also joins all array elements into a string.
+It behaves just like toString(), but in addition you can specify the separator:*/
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.join(" "); //Banana Orange Apple Mango
