96e83cadc9f395aa558bd1b6162431c9ed543bc0
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-17 11:49:42 +0530

    Update arrayInbuiltFunction.js

--- arrayInbuiltFunction.js
+++ arrayInbuiltFunction.js
@@ -24,3 +24,32 @@ let fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.push("Grapes"); //["Banana", "Orange", "Apple","Mango","Grapes"]
 //The push() method returns the new array length:
 let x= fruits.push("Grapes"); // x is 5
+
+
+/*Shifting Elements
+Shifting is equivalent to popping, working on the first element instead of the last.
+The shift() method removes the first array element and "shifts" all other elements to a lower index.*/
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push();//["Orange", "Apple", "Mango"]
+
+/*The shift() method returns the string that was "shifted out":*/
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.shift();    // the value of x is "Banana"
+
+
+/*The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:*/
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");    // Adds a new element "Lemon" to fruits
+
+//The unshift() method returns the new array length.
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");    // Returns 5
+
+/*Splicing an Array
+The splice() method can be used to add new items to an array:*/
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi"); // first parameter (2) defines the position where new elements should be added.Second parameter (0) defines how many elements should be removed.
+//output: Banana,Orange,Lemon,Kiwi,Apple,Mango
+
