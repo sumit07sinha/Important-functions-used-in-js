d542811a7ffcbf549409dc2f91246ac597b5101a
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-17 10:31:35 +0530

    popping and pushing

--- arrayInbuiltFunction.js
+++ arrayInbuiltFunction.js
@@ -8,3 +8,14 @@ It behaves just like toString(), but in addition you can specify the separator:*
 
 let fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("demo").innerHTML = fruits.join(" "); //Banana Orange Apple Mango
+
+/*Popping and Pushing
+The pop() method removes the last element from an array:*/
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop(); //["Banana", "Orange", "Apple"]
+
+//The push() method adds a new element to an array (at the end):
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Grapes"); //["Banana", "Orange", "Apple","Mango","Grapes"]
