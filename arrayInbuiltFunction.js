5e1fbc12b4a33d9398f7d14fb7c67b5bad7920ca
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-17 10:20:54 +0530

    toString() function

---
+++ arrayInbuiltFunction.js
@@ -0,0 +1,4 @@
+//The JavaScript method toString() converts an array to a string of (comma separated) array values.
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString(); //Banana,Orange,Apple,Mango
